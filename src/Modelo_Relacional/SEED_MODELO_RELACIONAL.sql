e6097f5cff1ace7bd135a0816ec19f4575df48d8
LuisElianM <luiselian001@gmail.com>
2024-06-04 21:57:01 -0400

    seed completed

--- src/Modelo_Relacional/SEED_MODELO_RELACIONAL.sql
+++ src/Modelo_Relacional/SEED_MODELO_RELACIONAL.sql
@@ -65,3 +65,107 @@ VALUES
     ('Seguro Menor de edad', 'Seguro de salud para personas menores a 18 años de edad', 1, 5),
     ('Seguro Adulto mayor', 'Seguro de salud para personas mayores a 60 años de edad', 1, 5),
     ('Seguro Adulto', 'Seguro de salud para personas meyores a 18 años y menores a 60 años', 1, 5);
+
+INSERT INTO "Modelo_Relacional"."EVALUACION_SERVICIO" (descripcion_evaluacion)
+VALUES 
+    ('1.- Malo'),
+    ('2.- Regular'),
+    ('3.- Bueno'),
+    ('4.- Muy Bueno'),
+    ('5.- Excelente');
+
+INSERT INTO "Modelo_Relacional"."RECOMIENDA" (codigo_cliente, codigo_evaluacion_servicio, codigo_producto, recomienda_amigo)
+VALUES
+    (1, 4, 1, true),
+    (2, 5, 2, true),
+    (3, 3, 3, false),
+    (4, 3, 1, false),
+    (5, 5, 2, true),
+    (6, 5, 3, true),
+    (7, 5, 1, true),
+    (8, 5, 2, true),
+    (9, 5, 3, true),
+    (10, 5, 1, true),
+    (11, 5, 2, true),
+    (12, 5, 3, true),
+    (13, 5, 1, true),
+    (14, 5, 2, true),
+    (15, 3, 3, false),
+    (16, 3, 1, false),
+    (17, 5, 2, true),
+    (18, 5, 3, true),
+    (19, 3, 1, false),
+    (20, 5, 2, true),
+    (21, 3, 3, false),
+    (22, 5, 1, true),
+    (23, 5, 2, true),
+    (24, 5, 3, true);
+
+INSERT INTO "Modelo_Relacional"."CONTRATO" (descripcion_contrato)
+VALUES 
+    ('Contrato de seguro de salud para personas menores a 18 años de edad'),
+    ('Contrato de seguro de salud para personas mayores a 60 años de edad'),
+    ('Contrato de seguro de salud para personas meyores a 18 años y menores a 60 años');
+
+INSERT INTO "Modelo_Relacional"."REGISTRO_CONTRATO" (numero_contrato, codigo_cliente, codigo_producto, fecha_inicio, fecha_fin, monto, estado_contrato)
+VALUES
+    (1, 1, 1, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (2, 2, 2, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (3, 3, 3, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (1, 4, 1, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (2, 5, 2, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (3, 6, 3, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (1, 7, 1, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (2, 8, 2, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (3, 9, 3, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (1, 10, 1, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (2, 11, 2, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (3, 12, 3, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (1, 13, 1, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (2, 14, 2, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (3, 15, 3, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (1, 16, 1, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (2, 17, 2, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (3, 18, 3, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (1, 19, 1, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (2, 20, 2, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (3, 21, 3, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (1, 22, 1, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (2, 23, 2, '2024-06-04', '2025-06-04', 1000.00, 'activo'),
+    (3, 24, 3, '2024-06-04', '2025-06-04', 1000.00, 'activo');
+
+INSERT INTO "Modelo_Relacional"."SINIESTRO" (descripcion_siniestro)
+VALUES 
+    ('Accidente de tránsito'),
+    ('Incendio'),
+    ('Robo'),
+    ('Hurto'),
+    ('Daños materiales'),
+    ('Daños personales');
+
+INSERT INTO "Modelo_Relacional"."REGISTRO_SINIESTRO" (numero_siniestro, numero_contrato, fecha_siniestro, fecha_respuesta, id_rechazo, monto_reconocido, monto_solicitado)
+VALUES 
+    (1, 1, '2024-06-06', '2024-06-06', 'no', 1000.00, 1000.00),
+    (2, 2, '2024-06-07', '2024-06-07', 'no', 1000.00, 1000.00),
+    (3, 3, '2024-06-08', '2024-06-08', 'no', 1000.00, 1000.00),
+    (4, 1, '2024-06-12', '2024-06-12', 'no', 1000.00, 1000.00),
+    (5, 2, '2024-06-14', '2024-06-14', 'no', 1000.00, 1000.00),
+    (6, 3, '2024-06-24', '2024-06-24', 'no', 100.00, 1000.00),
+    (1, 1, '2024-06-29', '2024-06-29', 'si', 100.00, 1000.00),
+    (2, 2, '2024-07-02', '2024-07-02', 'si', 100.00, 1000.00),
+    (3, 3, '2024-07-05', '2024-07-05', 'si', 150.00, 1000.00),
+    (4, 1, '2024-07-06', '2024-07-06', 'no', 1000.00, 1000.00),
+    (5, 2, '2024-07-07', '2024-07-07', 'no', 1000.00, 1000.00),
+    (6, 3, '2024-07-08', '2024-07-08', 'no', 1000.00, 1000.00),
+    (1, 1, '2024-07-12', '2024-07-12', 'no', 1000.00, 1000.00),
+    (2, 2, '2024-07-14', '2024-07-14', 'no', 1000.00, 1000.00),
+    (3, 3, '2024-07-24', '2024-07-24', 'no', 1000.00, 1000.00),
+    (4, 1, '2024-07-29', '2024-07-29', 'no', 1000.00, 1000.00),
+    (5, 2, '2024-08-06', '2024-08-06', 'no', 1000.00, 1000.00),
+    (6, 3, '2024-08-07', '2024-08-07', 'no', 1000.00, 1000.00),
+    (1, 1, '2024-08-08', '2024-08-08', 'no', 1000.00, 1000.00),
+    (2, 2, '2024-08-12', '2024-08-12', 'no', 1000.00, 1000.00),
+    (3, 3, '2024-08-14', '2024-08-14', 'no', 1000.00, 1000.00),
+    (4, 1, '2024-08-24', '2024-08-24', 'si', 200.00, 1000.00),
+    (5, 2, '2024-08-29', '2024-08-29', 'si', 120.00, 1000.00),
+    (6, 3, '2024-09-04', '2024-09-04', 'si', 500.00, 1000.00);
